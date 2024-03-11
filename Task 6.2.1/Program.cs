0ac1120e5aa9cff86f37bc10fd76f572acd9d7c1
Aleksei Adamenko <adamenko.aleksei81@gmail.com>
2024-03-12 02:45:56 +0300

    Task 6.2.1 Done

---
+++ Task 6.2.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringSource = Console.ReadLine();
+            string[] stringArray = stringSource.Split(' ');
+            string maxLenString = "";
+            foreach (string str in stringArray)
+            {
+                if (str.Length > maxLenString.Length)
+                {
+                    maxLenString = str;
+                }
+            }
+
+            Console.WriteLine(maxLenString);
+            Console.ReadKey();
+        }
+    }
+}
