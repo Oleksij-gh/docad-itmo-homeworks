9a6dece133844deeb8fd3fb76bfd8ac59042504c
Aleksei Adamenko <adamenko.aleksei81@gmail.com>
2024-03-12 20:53:33 +0300

    Task 10.2.1 Done

---
+++ Task 10.2.1/Edge.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10._2._1
+{
+    class Edge
+    {
+        int gradus;
+        int min;
+        int sec;
+
+        public int Gradus
+        {
+            get{
+                return gradus;
+            }
+            set{
+                gradus = value % 360;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                Gradus += value / 60;
+                min = value % 60;
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                Min += value / 60;
+                sec = value % 60;
+            }
+        }
+
+        public Edge(int gradus, int min, int sec)
+        {
+            this.gradus = gradus;
+            this.min = min;
+            this.sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            return Math.PI * (gradus + (min + sec / 60) / 60) / 180;
+        }
+    }
+}
---
+++ Task 10.2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gradus, min, sec;
+            Console.Write("Введите градусы: ");
+            gradus = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите минуты: ");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите секунды: ");
+            sec = Convert.ToInt32(Console.ReadLine());
+
+            Edge edge = new Edge(gradus, min, sec);
+            Console.WriteLine("При переводе в радианы: {0:F5}", edge.ToRadians());
+            Console.ReadKey();
+        }
+    }
+}
