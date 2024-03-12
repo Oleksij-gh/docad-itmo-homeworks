b89cd78e6ab0dd81ae9d16baf897f8b503c9c6ea
Aleksei Adamenko <adamenko.aleksei81@gmail.com>
2024-03-12 04:26:05 +0300

    Task 8.2.1 Done

---
+++ Task 8.2.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_8._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример для дирректории проекта
+            //string path = Directory.GetCurrentDirectory();
+            string path = "C:\\study\\stat";
+            GetFiles(path);
+            Console.ReadKey();
+
+
+
+        }
+
+        static void GetFiles(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            DirectoryInfo[] subDirs = dir.GetDirectories();
+            FileInfo[] files = dir.GetFiles();
+            Console.WriteLine("dir: {0}", dir.FullName);
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine("---file: {0}",file.FullName);
+            }
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                GetFiles(subDir.FullName);
+            }
+        }
+    }
+}
