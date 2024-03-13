be6a513a75d0cada0c4d047d87a7fc5dff8bf543
Aleksei Adamenko <adamenko.aleksei81@gmail.com>
2024-03-14 01:07:30 +0300

    Task 11.2.1 Done

---
+++ Task 11.2.1/Equation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11._2._1
+{
+    internal struct Equation
+    {
+        double k;
+        double b;
+
+        public Equation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        
+        public string Root()
+        {
+            if (k == 0)
+            {
+                return "Решений нет";
+            }
+            else
+            {
+                return $"Решение уравнения: x = {-b / k}";
+            }
+        }
+    }
+}
---
+++ Task 11.2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double k, b;
+            Console.WriteLine("Для уравнения вида 0 = kx + b");
+            Console.Write("Введите коэффициент k = ");
+            k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите коэффициент b = ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Equation equation = new Equation(k, b);
+            Console.WriteLine(equation.Root()); 
+            Console.ReadKey();
+
+
+        }
+    }
+}
