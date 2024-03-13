e863d336f5e680a9514421562c9deb807eeeae29
Aleksei Adamenko <adamenko.aleksei81@gmail.com>
2024-03-14 02:02:53 +0300

    Task 12.2.1 Done

---
+++ Task 12.2.1/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12._2._1
+{
+    static class Circle
+    {
+        static public double Lenght(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        static public double Square(double radius)
+        {
+            return Math.PI * radius;
+        }
+
+        static public bool PointInCircle(double x, double y, double radius)
+        {
+            double hypotenuse = Math.Sqrt(x * x + y * y);
+            return hypotenuse <= radius;
+        }
+    }
+}
---
+++ Task 12.2.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, R;
+            Console.WriteLine("Введите координаты точки (x, y) и радиус окружности R.");
+            Console.Write("x = ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y = ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("R = ");
+            R = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Длина окружности: {Circle.Lenght(R)}\nПлощадь окружности: {Circle.Square(R)}");
+
+            if (Circle.PointInCircle(x, y, R)){
+                Console.WriteLine("Точка находится в окружности.");
+            }
+            else
+            {
+                Console.WriteLine("Точка находится вне окружности.");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
