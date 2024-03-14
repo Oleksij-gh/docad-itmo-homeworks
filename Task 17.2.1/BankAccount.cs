ae190c1e52c83e1bb698e8a1676ed3896481e2b3
Aleksei Adamenko <adamenko.aleksei81@gmail.com>
2024-03-14 08:27:38 +0300

    Task 17.2.1 Done

---
+++ Task 17.2.1/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_17._2._1
+{
+    internal class BankAccount<T>
+    {
+        public T AccountNumber { get; set; }
+        public double Balance { get; set; }
+        public string Fio { get; set; }
+
+
+        public string GetInfo()
+        {
+            return $"Номер аккаунта: {AccountNumber}\nФИО владельца: {Fio}\nБаланс: {Balance}";
+        }
+
+        public void InputInfo()
+        {
+            Console.Write("Введите номер аккаунта: ");
+            AccountNumber = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.Write("Введите баланс аккаунта: ");
+            Balance = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите ФИО владельца аккаунта: ");
+            Fio = Console.ReadLine();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Task 17.2.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_17._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BankAccount<int> bankAccountInt = new BankAccount<int>();
+            bankAccountInt.InputInfo();
+            Console.WriteLine(bankAccountInt.GetInfo());
+            Console.WriteLine();
+
+            BankAccount<string> bankAccountString = new BankAccount<string>();
+            bankAccountString.InputInfo();
+            Console.WriteLine(bankAccountString.GetInfo());
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
