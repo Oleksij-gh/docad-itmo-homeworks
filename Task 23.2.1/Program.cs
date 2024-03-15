1332ddd6bf9b146cb13159b2982a39aa3d705ed3
Aleksei Adamenko <adamenko.aleksei81@gmail.com>
2024-03-15 21:06:45 +0300

    Task 23.2.1 Done

---
+++ Task 23.2.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_23._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Введите n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            FactorialAsync(n);
+            Console.ReadKey();
+        }
+
+        static void Factorial(int n)
+        {
+            int factorial = 1;
+            for (int i = 1; i < n; i++)
+            {
+                factorial *= i;
+                Thread.Sleep(10);
+            }
+            Console.WriteLine(factorial);
+        }
+
+        static async void FactorialAsync(int n)
+        {
+            await Task.Run(() => Factorial(n));
+        }
+    }
+}
